09faf0c4c48ddb8b896ac35975c89d6fcc5dd255
Peter <pcebaker@gmail.com>
2020-07-28 15:00:07 +0100

    Provide an interface for a basic call to DarkSky API via a proxy to strip CORS restrictions

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from  "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  // We must utilise a proxy to easily work around a CORS rejection from DarkSky, cors-anywhere is a free service that provides this.
+  private corsProxy = 'https://cors-anywhere.herokuapp.com';
+  // DarkSky API URL and Key (Ideally this should be kept private and not shared with the client)
+  private darkSky = 'https://api.darksky.net/forecast/1a43d545710c14571dbbe87b13bad8c7';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public fetchWeatherReport(latitude, longitude) {
+    this.http.get(`${this.corsProxy}/${this.darkSky}/${latitude},${longitude}`)
+      .subscribe(
+        data => {
+          console.log("Weather data retrieved successfully ", data);
+        },
+        error => {
+          console.log("An error while fetching error data", error);
+        }
+      );
+  }
 }
