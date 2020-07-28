4664ead11065c566675c99902baa60a9f6fc09fc
Peter <pcebaker@gmail.com>
2020-07-28 22:16:24 +0100

    Weather service and tests for data fetching, validation, and formatting

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -17,5 +17,147 @@ describe('WeatherService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  
+  it('#fetchWeatherReport() should return false if passed invalid coordinates', (done) => {
+    const latitude = 9999;
+    const longitude = -9999;
+
+    const mockData = {};
+
+    service.fetchWeatherReport(latitude, longitude).subscribe(data => {
+      expect(data).toBe(false);
+      done();
+    });
+  });
+
+  it('#fetchWeatherReport() should resolve as an observable', () => {
+    const latitude = 51.505909;
+    const longitude = -0.023220;
+
+    const mockData = {};
+
+    const fetchWeatherReport = service.fetchWeatherReport(latitude, longitude);
+    expect(fetchWeatherReport).toEqual(jasmine.any(Observable));
+  });
+
+  it('#fetchWeatherReport() observable should resolve to object', () => {
+    const latitude = 51.505909;
+    const longitude = -0.023220;
+
+    const mockData = {};
+
+    service.fetchWeatherReport(latitude, longitude).subscribe(report => {
+      expect(report).toEqual(jasmine.any(Object));
+    });
+
+    const req = httpMock.expectOne('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1a43d545710c14571dbbe87b13bad8c7/51.505909,-0.02322');
+
+    req.flush(mockData);
+
+    httpMock.verify();
+  });
+
+  it('#validateWeatherResponse() should return false if passed an incomplete weather report', () => {
+    const invalidReport = {
+      daily: {
+        data: [{
+          time: 1596150000,
+          icon: 'rain',
+          temperatureHigh: 24,
+          temperatureLow: 15
+        }]
+      }
+    };
+    const result = service.validateWeatherResponse(invalidReport);
+
+    expect(result).toBe(false);
+  });
+
+  it('#validateWeatherResponse() should return true if passed a complete weather report', () => {
+    const validReport = {
+      currently:{
+        humidity: 0.47,
+        icon: "partly-cloudy-day",
+        precipProbability: 0,
+        temperature: 64.82,
+        time: 1595963789,
+        windSpeed: 9.97,
+      },
+      daily: {
+        data: [
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+        ]
+      }
+    };
+    const result = service.validateWeatherResponse(validReport);
+
+    expect(result).toBe(true);
+  });
+
+  it('#formatWeatherResponse() should convert daily unix timestamps into identifiable day strings', () => {
+    const validReport = {
+      currently:{
+        humidity: 0.47,
+        icon: "partly-cloudy-day",
+        precipProbability: 0,
+        temperature: 64.82,
+        time: 1595963789,
+        windSpeed: 9.97,
+      },
+      daily: {
+        data: [
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+        ]
+      }
+    };
+    const result = service.formatWeatherResponse(validReport);
+
+    const currentDayInWeek = result['week'][0]['day'];
+
+    expect(currentDayInWeek).toBe('Fri');
+  });
+
+  it('#formatWeatherResponse() should assign the current temperateHigh and temperatureLow values based on the daily values', () => {
+    const validReport = {
+      currently:{
+        humidity: 0.47,
+        icon: "partly-cloudy-day",
+        precipProbability: 0,
+        temperature: 64.82,
+        time: 1595963789,
+        windSpeed: 9.97,
+      },
+      daily: {
+        data: [
+          { time: 1596150000, icon: 'rain', temperatureHigh: 24, temperatureLow: 15 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 32, temperatureLow: 11 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 27, temperatureLow: 18 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 26, temperatureLow: 16 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 31, temperatureLow: 28 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 29, temperatureLow: 19 },
+          { time: 1596150000, icon: 'rain', temperatureHigh: 23, temperatureLow: 17 },
+        ]
+      }
+    };
+    const result = service.formatWeatherResponse(validReport);
+
+    const temperatureHigh = result['current']['temperatureHigh'];
+    const temperatureLow = result['current']['temperatureLow'];
+
+    expect(temperatureHigh).toBe(24);
+    expect(temperatureLow).toBe(15);
+  });
 
 });
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from  "@angular/common/http";
+import { of, Observable } from 'rxjs';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +13,72 @@ export class WeatherService {
   // DarkSky API URL and Key (Ideally this should be kept private and not shared with the client)
   private darkSky = 'https://api.darksky.net/forecast/1a43d545710c14571dbbe87b13bad8c7';
 
+  private daysToDisplay: number = 7;
+
   constructor(private http: HttpClient) { }
 
-  public fetchWeatherReport(latitude, longitude) {
-    this.http.get(`${this.corsProxy}/${this.darkSky}/${latitude},${longitude}`)
-      .subscribe(
-        data => {
-          console.log("Weather data retrieved successfully ", data);
-        },
-        error => {
-          console.log("An error while fetching error data", error);
-        }
-      );
+  public fetchWeatherReport(latitude: number, longitude: number): Observable<any> {
+    if (latitude < -90 || latitude > 90) return of(false);
+    if (longitude < -180  || longitude > 180) return of(false);
+
+    return this.http.get(`${this.corsProxy}/${this.darkSky}/${latitude},${longitude}`, {headers: {'X-Requested-With' :'XMLHttpRequest'}});
   }
+  
+  public validateWeatherResponse(response: object): boolean {
+    // Ensure the response has all keys required to determine current weather
+    if (response.hasOwnProperty('currently')) {
+
+      const currentRequiredKeys = [ 'time', 'icon', 'temperature', 'precipProbability', 'windSpeed', 'humidity' ];
+      for (let i = 0; i < currentRequiredKeys.length; i++) {
+        if (!response['currently'].hasOwnProperty(currentRequiredKeys[i])) {
+          // console.log("Unable to determine current value for ", currentRequiredKeys[i]);
+          return false;
+        }
+      };
+    } else {
+      return false;
+    }
+
+    // Ensure the response has all keys required to determine forecasted weather
+    if (response.hasOwnProperty('daily')) {
+      // There must be daily data available
+      if (response['daily']['data'].length < this.daysToDisplay) return false;
+
+      const dailyRequiredKeys = [ 'time', 'icon', 'temperatureHigh', 'temperatureLow' ];
+      for (let i = 0; i < dailyRequiredKeys.length; i++) {
+        for (let x = 0; x < this.daysToDisplay; x++) {
+          if (!response['daily']['data'][x].hasOwnProperty(dailyRequiredKeys[i])) {
+            // console.log("Unable to determine forecasted value for ", dailyRequiredKeys[i]);
+            return false;
+          }
+        }
+      };
+    } else {
+      return false;
+    }
+
+    return true;
+  }
+
+  public formatWeatherResponse(response: object): object {
+
+    let weatherReport = {};
+
+    weatherReport['current'] = response['currently'];
+    weatherReport['current']['day'] = moment.unix(weatherReport['current']['time']).format('ddd');
+    weatherReport['current']['temperatureHigh'] = response['daily']['data'][0]['temperatureHigh'];
+    weatherReport['current']['temperatureLow'] = response['daily']['data'][0]['temperatureLow'];
+
+    weatherReport['week'] = response['daily']['data'].slice(0, this.daysToDisplay);
+
+    for (let i = 0; i < weatherReport['week'].length; i++) {
+      const dayName = moment.unix(weatherReport['week'][i]['time']).format('ddd');
+      weatherReport['week'][i]['day'] = dayName;
+    }
+
+    // console.log('Trimmed data structure: ', weatherReport);
+
+    return weatherReport;
+  }
+
 }
